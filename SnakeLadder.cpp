22178f5909279d31108c8b2dd65362e625a02692
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-12-29 14:17:27 +0530

    define constructor for game class

---
+++ SnakeLadder.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Game {
+    Board board;
+    Dice dice;
+    vector<Player> players;
+    int currentTurn;
+
+public:
+    Game(int boardSize = 100) : board(boardSize){
+        currentTurn = 0;
+        srand(time(0));
+    }
+    
+      
+}
+int main(){
+
+}
