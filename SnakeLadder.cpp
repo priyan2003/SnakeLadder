a1db96e4e0f11355e488cf932cbd19f4b450bbef
priyanshu <priyanshu.2201124cs@iiitbh.ac.in>
2025-12-29 14:59:37 +0530

    complete board class implementation

--- SnakeLadder.cpp
+++ SnakeLadder.cpp
@@ -10,7 +10,26 @@ public:
     Board(int n = 100){
         size = n;
     }
-    
+    void addSnake(int head, int tail){
+        snakes[head] = tail;
+    }
+    void addLadder(int start, int end){
+        ladders[start] = end;
+    }
+    int getFinalPosition(int pos){
+        if(snakes.count(pos)){
+            cout<<"Snake down from "<<pos<<" to "<<snakes[pos]<<endl;
+            return snakes[pos];
+        }
+        if(ladders.count(pos)){
+            cout<<"Ladder up from "<<pos<<" to "<<ladders[pos]<<endl;
+            return ladders[pos];
+        }
+        return pos;
+    }
+    int getSize() const{
+        return size;
+    }
 };
 
 class Game {
